bce1a9b8bb742b122172ccfa8893bf75df702f3f
weixiaochuan <18838958405@126.com>
2020-04-17 15:02:31 +0800

    feat: 编写分类页面

--- pages/class/class.js
+++ pages/class/class.js
@@ -4,16 +4,37 @@ const app = getApp()
 
 Page({
 	data: {
-		arr: [
+		list: [
 			{
-				d: 1
+				name: '冰箱',
+				chilren: [
+					{
+						name: '格力冰箱',
+						chilren: [
+							{
+								name: '三门冰箱',
+							},
+						],
+					},
+				],
 			},
-			{
-				d: 13
-			},
-		]
+		],
 	},
 	onLoad: function () {
 
 	},
-})
+
+	// 跳转搜索页面
+	jumpToSearchPage() {
+		wx.navigateTo({
+			url: '../search/search',
+		});
+	},
+
+	// 跳转消息页面
+	jumpNewsPage() {
+		wx.navigateTo({
+			url: '../news/news',
+		});
+	},
+});
